07e117e1cba622b1f8b53d5ed7df0ec9aa8f82d7
Priscca <priscillaffd@gmail.com>
2022-03-30 21:04:01 -0300

    Sobre comentários

---
+++ inicio/comentários.js
@@ -0,0 +1,5 @@
+//console.log é usado para exibir algo na tela do computador - explicação/observação
+
+console.log("Passo 01");
+console.log("Passo 02"); //mais um exemplo de console.log comentar uma sentaça
+//console.log("Passo 03"); sentença de código invalida
